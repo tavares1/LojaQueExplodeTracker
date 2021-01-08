25877c9f63990147c6b97365ba85cc19f61935d1
Lucas Tavares <lucastavaresvasconcelos@gmail.com>
2021-01-08 01:14:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const axios = require("axios");
+const qs = require("qs");
+
+class FedexService {
+  fetchOrderID(nrIdentificao, nrDocumento) {
+    return new Promise((resolve, reject) => {
+      const data = {
+        remDest: "D",
+        nrIdentificacao: nrIdentificao,
+        idFilial: "",
+        tpDocumento: "NF",
+        nrDocumento: nrDocumento,
+      };
+
+      const BASE_URL_ID =
+        "https://radar.tntbrasil.com.br/radar/public/localizacaoSimplificada/search";
+
+      axios({
+        method: "post",
+        url: BASE_URL_ID,
+        data: qs.stringify(data),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
+      })
+        .then((response) => {
+          console.log(response);
+          if (response.data.aaData.length > 0) {
+            const orderID = response.data.aaData[0];
+            resolve(orderID.id)
+          } else {
+            reject('Credenciais invalidas')
+          }
+        })
+        .catch((error) => reject(error));
+    });
+  }
+
+  fetchDocumentID(orderID) {
+    return new Promise((resolve, reject) => {
+      axios
+      .get(
+        `https://radar.tntbrasil.com.br/radar/public/localizacaoSimplificadaDetail/${orderID}`
+      )
+      .then((response) => {
+        const html = response.data;
+        let index = html.indexOf("idDocumento");
+        let idDocumento = [];
+        while (html[index] != ",") {
+          idDocumento.push(html[index]);
+          index++;
+        }
+        idDocumento = idDocumento.join("");
+        let finalPosition = idDocumento.length;
+        let initialPosition = idDocumento.indexOf(":");
+        idDocumento = idDocumento.substring(initialPosition + 2, finalPosition);
+        resolve(idDocumento)
+      })
+      .catch((error) => {
+        reject(error)
+      });
+    })
+  }
+
+  actualStatusOrder(id) {
+    return new Promise((resolve, reject) => {
+      axios
+      .get(
+        `https://radar.tntbrasil.com.br/radar/service/tracking/findLinhaTempoEvento?idDoctoServico=${id}`
+      )
+      .then((response) => {
+        resolve(response.data)
+      })
+      .catch((error) => {
+        reject(error)
+        })
+    })
+  }
+}
+
+module.exports = FedexService;
---
+++ server.js
@@ -0,0 +1,53 @@
+const app = require('express')()
+const bodyParser = require('body-parser')
+const FedexService = require('./index')
+
+const service = new FedexService()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/fetchOrderID', async (req,res,next) => {
+    const {nrDocumento, nrIdentificacao} = req.body
+    try {
+        let orderID = await service.fetchOrderID(nrIdentificacao,nrDocumento)
+        res.json({
+            'orderID':orderID
+        })
+        next()
+    } catch (error) {
+        res.json(error)
+        next()
+    }
+})
+
+app.get('/fetchDocumentID', async (req,res,next) => {
+    const {orderID} = req.query
+    try {
+        let documentID = await service.fetchDocumentID(orderID)
+        res.json({
+            'documentID':documentID
+        }) 
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+app.get('/fetchStatusOrder', async (req,res,next) => {
+    const {documentID} = req.query
+    try {
+        let actualStatus = await service.actualStatusOrder(documentID)
+        res.json({
+            'status':actualStatus
+        })
+    } catch ( error ) {
+        res.json(error)
+    }
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
